ee062ed4c5e23d422377340eacf8c7babbc0de15
Sam Kenny <me@samk3nny.com>
2013-12-27 16:30:20 -0500

    hello.py changed to dynamically assign port

--- hello.py
+++ hello.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-#app.run(host='192.168.1.12', port=8080)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
     
 if __name__ == '__main__':
-    app.run()    
+# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
