efbe637ec768b0358452bb7bbcd7df4e51c536c4
Sam Kenny <me@samk3nny.com>
2013-12-27 15:36:19 -0500

    re-init

---
+++ hello.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+#app.run(host='192.168.1.12', port=8080)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+    
+if __name__ == '__main__':
+    app.run()    
